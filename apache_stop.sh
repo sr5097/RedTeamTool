0a14bae7b63d550f40c072bf1b723b154195748f
Salomi Rao <49054022+sr5097@users.noreply.github.com>
2023-02-15 15:29:56 -0500

    Update apache_stop.sh

--- apache_stop.sh
+++ apache_stop.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-while true
+: '
+The script sleeps for a random amount of time between 1 and 500 seconds
+using the sleep command, and then stops the Apache service using the sudo
+command. This loop will continue to run indefinitely until the script is 
+interrupted or terminated.
+'
+while true		# Starts an infinite loop
 do
-	sleep $((RANDOM % 10 + 1))
-	sudo /usr/sbin/service apache2 stop
+	sleep $((RANDOM % 500 + 1))		# Sleep for a random amount of time (between 1 and 500 seconds)
+	# Stop the Apache service using the sudo command
+	sudo /usr/sbin/service apache2 stop		
 done
